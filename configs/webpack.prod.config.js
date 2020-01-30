2c5909fbec8929503c7c994c2de2245d1491603b
Yaroslav Vertukhov <yvertukhov@extyl-pro.ru>
2020-01-30 17:26:32 +0300

    fix prod config

--- configs/webpack.prod.config.js
+++ configs/webpack.prod.config.js
@@ -1,10 +1,16 @@
 //prod config
 
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const baseWebpackConfig = require('./webpack.base.config.js');
 
 const buildWebpackConfig = merge(baseWebpackConfig, {
-    mode: 'production'
+    mode: 'production',
+    plugins: [
+        new webpack.SourceMapDevToolPlugin({
+            filename: '[file].map'
+        })
+    ]
 })
 
 module.exports = buildWebpackConfig
