c99804aab7ea62d81ccf3a1517a73633e912f711
Yaroslav <yarosalv-vertukhov@yandex.ru>
2020-02-23 16:02:08 +0300

    add postcss

---
+++ configs/postcss.config.js
@@ -0,0 +1,9 @@
+const autoprefixer = require('autoprefixer');
+
+// module.exports = {
+//     plugins: [autoprefixer({ grid: true }), require("cssnano")]
+// };
+
+module.exports = {
+    plugins: [autoprefixer()]
+};
--- configs/webpack.dev.config.js
+++ configs/webpack.dev.config.js
@@ -18,10 +18,10 @@ const devWebpackConfig = merge(baseWebpackConfig, {
                         loader: 'css-loader',
                         options: { sourceMap: true }
                     },
-                    // {
-                    //    loader: 'postcss-loader',
-                    //    options: { sourceMap: true, config: { path: `${PATHS.config}/postcss.config.js` } }
-                    // },
+                    {
+                       loader: 'postcss-loader',
+                       options: { sourceMap: true, config: { path: `./configs/postcss.config.js` } }
+                    },
                     {
                         loader: 'sass-loader',
                         options: { sourceMap: true }
