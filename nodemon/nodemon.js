a060137efc6657304f8de79497a560fc1e2a7d49
Yaroslav <yarosalv-vertukhov@yandex.ru>
2020-02-24 14:36:57 +0300

    fix watcher name

--- nodemon/nodemon.js
+++ nodemon/nodemon.js
@@ -2,21 +2,21 @@ var nodemon = require('nodemon');
 const chokidar = require('chokidar');
 
 
-let obj = chokidar.watch('./src/pug/', {ignored: /(^|[\/\\])\../});
+let watcher = chokidar.watch('./src/pug/', {ignored: /(^|[\/\\])\../});
 
-obj.on('ready', (event, path) => {
+watcher.on('ready', (event, path) => {
   nodemon({
     script: './server/server.js',
     ext: 'pug',
     ignore: "*.*",
   });
 
-  obj.on('add', (event, path) => {
+  watcher.on('add', (event, path) => {
     console.log("Произошло добавление Pug файла, перезагружаем сервер");
     nodemon.emit('restart');
   });
 
-  obj.on('unlink', (event, path) => {
+  watcher.on('unlink', (event, path) => {
     console.log("Произошло удаление Pug файла, перезагружаем сервер");
     nodemon.emit('restart');
   });
